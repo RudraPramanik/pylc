8e67ccb0aabf97b6341cf9547f4056c24c5bcfa4
Rudra <rudrahimangshoo2020@gmail.com>
2024-03-30 18:25:32 +0600

    cookie-parser

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+const app = express();
+
 
 dotenv.config({
     path:'./env'
 })
 
 connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000, ()=>{
+    console.log(`server is running at port:${process.env.PORT}`)
+  })
+})
+.catch((err)=>{
+  console.log("mongodb connection fail",err)
+})
 
 
 
